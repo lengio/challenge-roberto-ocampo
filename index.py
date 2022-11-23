d963e27a79dd6857789570d5770178733c96d35e
robocvi <robertoov@ciencias.unam.mx>
2022-11-22 19:16:21 -0600

    Complete project! :)

--- index.py
+++ index.py
@@ -1,5 +1,16 @@
+from src.http import getResponseFake, postUserSessionsFake
+from src.app import sortActivitiesByDateAndUserId, createListOfUserSessions
+
 def main():
-    print("Hello Slang!")
+    '''Complete algorithm for the slang challenge.
+        Complexity time for this algorithm is O(n*log n) with n total activities,
+        because we sorted the list, and that was the "slowest" procedure in all the code.'''
+
+    activity_list = getResponseFake()
+    user_dict = sortActivitiesByDateAndUserId(activity_list)
+    user_sessions = createListOfUserSessions(user_dict)
+
+    postUserSessionsFake({"user_sessions": user_sessions})
 
 if __name__ == "__main__":
     main()
---
+++ src/http.py
@@ -0,0 +1,37 @@
+import requests
+from tests.test_case import EXAMPLE
+from src.token import TOKEN
+
+def getResponseFake():
+   '''Function that help us test code functionality without doing any http requests,
+      it uses the test case above.'''
+   
+   return EXAMPLE
+
+def getActivitiesResponse():
+   '''Retrieve all activities from the slang page.'''
+
+   activities_response = requests.get(
+      "https://api.slangapp.com/challenges/v1/activities",
+      headers={"Authorization": TOKEN}
+      )
+   
+   return activities_response.json()
+
+def postUserSessions(user_sessions):
+   '''Post the resulting user_sessions to slang web page.'''
+
+   requests.post(
+      "https://api.slangapp.com/challenges/v1/sessions",
+      headers={"Authorization": TOKEN},
+      json=user_sessions
+      )
+
+   print("Done! :)")
+
+def postUserSessionsFake(user_sessions):
+   '''Function that prints the result of all the code.'''
+
+   print(user_sessions)
+   print("Done! :)")
+
